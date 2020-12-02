33e1e343f72dde7e2c5e474df5a2820ce000b37b
Juha-Matti Santala <juhamattisantala@gmail.com>
2020-12-02 19:58:31 +0200

    Autoformat day02.rs

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,5 +1,5 @@
-use std::io::{self, Read};
 use regex::Regex;
+use std::io::{self, Read};
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
@@ -9,10 +9,12 @@ fn main() -> io::Result<()> {
     let input2: Vec<&str> = input.clone();
 
     println!("Part 1: {:?}", validate_passwords(input).unwrap());
-    println!("Part 2: {:?}", validate_passwords_positional(input2).unwrap());
+    println!(
+        "Part 2: {:?}",
+        validate_passwords_positional(input2).unwrap()
+    );
 
     Ok(())
-
 }
 
 fn validate_passwords(input: Vec<&str>) -> Option<u32> {
@@ -37,7 +39,12 @@ fn validate_passwords(input: Vec<&str>) -> Option<u32> {
 ///
 /// Requirements:
 /// the amount of `inspection_character` in the password must be between `min` and `max`, inclusive
-fn is_valid_password_old(password: &str, inspection_character: &str, min: usize, max: usize) -> bool {
+fn is_valid_password_old(
+    password: &str,
+    inspection_character: &str,
+    min: usize,
+    max: usize,
+) -> bool {
     let valid_characters: usize = password.matches(inspection_character).count();
 
     min <= valid_characters && valid_characters <= max
@@ -56,7 +63,9 @@ fn validate_passwords_positional(input: Vec<&str>) -> Option<u32> {
             let first_match = &password.clone().nth(pos1 - 1)?;
             let second_match = &password.clone().nth(pos2 - 1)?;
 
-            if (first_match == chr && second_match != chr) || (first_match != chr && second_match == chr) {
+            if (first_match == chr && second_match != chr)
+                || (first_match != chr && second_match == chr)
+            {
                 valid += 1;
             }
         }
@@ -71,13 +80,13 @@ mod tests {
 
     #[test]
     fn it_solves_part1_example() {
-        let example_input = ["1-3 a: abcde","1-3 b: cdefg","2-9 c: ccccccccc"].to_vec();
+        let example_input = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"].to_vec();
         assert_eq!(validate_passwords(example_input).unwrap(), 2);
     }
 
     #[test]
     fn it_solves_part2_example() {
-        let example_input = ["1-3 a: abcde","1-3 b: cdefg","2-9 c: ccccccccc"].to_vec();
+        let example_input = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"].to_vec();
         assert_eq!(validate_passwords_positional(example_input).unwrap(), 1);
     }
 }
