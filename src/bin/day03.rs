2c7984560a82b070c90761ae022d15f30dc7ea51
Juha-Matti Santala <juhamattisantala@gmail.com>
2020-12-03 17:20:27 +0200

    Solved day 3

---
+++ src/bin/day03.rs
@@ -0,0 +1,132 @@
+use std::io::{self, Read};
+
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+
+    let input: Vec<&str> = buffer.lines().collect();
+
+    println!("Part 1: {:?}", first_part(&input).unwrap());
+    println!(
+        "Part 2: {:?}",
+        second_part(&input, &vec![
+            (1, 1),
+            (3, 1),
+            (5, 1),
+            (7, 1),
+            (1, 2)
+        ]).unwrap()
+    );
+
+    Ok(())
+}
+
+/// Calculates how many # characters are in the line of movement
+/// When advancing in the 2D matrix by the rule of
+/// `slope.1` row forward, `slope.0` columns forward
+/// 
+/// The input field will repeat to infinity to the right with copying
+/// the pattern before it:
+/// 
+/// '#..#..#....#' is same as
+/// '#..#..#....##..#..#....##..#..#....##..#..#....##..#..#....##..#..#....#' to infinity
+fn solve_slope(input: &Vec<&str>, slope: &(usize, usize)) -> Option<usize> {
+    let mut row: usize = 0;
+    let mut col: usize = 0;
+    
+    let max_rows: usize = input.len();
+    let max_cols: usize = input[0].len();
+
+    let mut trees: usize = 0;
+
+    loop {
+        row += slope.1;
+        col += slope.0;
+
+        if row >= max_rows {
+            break;
+        }
+
+        // The input repeats to infinity in columns with the same pattern
+        // so we need to loop back to beginning once we go over
+        if col >= max_cols {
+            col -= max_cols;
+        }
+
+        let chr = input[row].chars().nth(col)?;
+
+        if chr == '#' {
+            trees += 1;
+        }
+    }
+    Some(trees)
+}
+
+/// Solve the slope for single slope of 3, 1
+fn first_part(input: &Vec<&str>) -> Option<usize> {
+    solve_slope(&input, &(3, 1))
+}
+
+/// The product of slope difficulties for multiple slopes
+fn second_part(input: &Vec<&str>, slopes: &Vec<(usize, usize)>) -> Option<usize> {
+    let mut slope_difficulties = vec![];
+    for slope in slopes {
+        slope_difficulties.push(solve_slope(&input, &slope).unwrap());
+    }
+
+    let mut slope_difficulty: usize = 1;
+    for difficulty in slope_difficulties {
+        slope_difficulty *= difficulty;
+    }
+
+    Some(slope_difficulty)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_solves_part1_example() {
+        let example_input = [
+            "..##.........##.........##.........##.........##.........##.......",
+            "#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..",
+            ".#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.",
+            "..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#",
+            ".#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.",
+            "..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....",
+            ".#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#",
+            ".#........#.#........#.#........#.#........#.#........#.#........#",
+            "#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...",
+            "#...##....##...##....##...##....##...##....##...##....##...##....#",
+            ".#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#",
+        ].to_vec();
+        assert_eq!(first_part(&example_input).unwrap(), 7);
+    }
+
+    #[test]
+    fn it_solves_part2_example() {
+        let example_input = [
+            "..##.........##.........##.........##.........##.........##.......",
+            "#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..",
+            ".#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.",
+            "..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#",
+            ".#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.",
+            "..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....",
+            ".#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#",
+            ".#........#.#........#.#........#.#........#.#........#.#........#",
+            "#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...",
+            "#...##....##...##....##...##....##...##....##...##....##...##....#",
+            ".#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#",
+        ].to_vec();
+        let example_slopes = vec![
+            (1, 1),
+            (3, 1),
+            (5, 1),
+            (7, 1),
+            (1, 2)
+        ];
+        assert_eq!(second_part(&example_input, &example_slopes).unwrap(), 336);
+    }
+}
